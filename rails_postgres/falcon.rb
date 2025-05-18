e32a1cca26328604611553260c0d585bffe31b6a
ItaloCobains <italobrandao.contato@gmail.com>
2025-05-18 16:19:13 -0300

    Update Dockerfile, Gemfile, and Nginx configuration for Falcon integration; remove Puma references and adjust service ports

---
+++ rails_postgres/falcon.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S falcon host
+# frozen_string_literal: true
+
+require "falcon/environment/rack"
+
+hostname = File.basename(__dir__)
+port = ENV["PORT"] || 80
+
+
+service hostname do
+	preload "preload.rb"
+	include Falcon::Environment::Rack
+
+	endpoint Async::HTTP::Endpoint.parse("http://0.0.0.0:#{port}")
+end
