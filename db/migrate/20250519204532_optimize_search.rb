d53ee1d1a2e75e13bc77ec0e43bfd11e858e55cc
Wagner Caixeta <wagner@baladapp.com.br>
2025-05-19 19:44:22 -0300

    commit migrate down

--- db/migrate/20250519204532_optimize_search.rb
+++ db/migrate/20250519204532_optimize_search.rb
@@ -11,4 +11,14 @@ class OptimizeSearch < ActiveRecord::Migration[8.0]
         USING GIST (lower(apelido || nome || stack::text) gist_trgm_ops);
     SQL
   end
+
+  def down
+    execute <<~SQL
+      DROP INDEX IF EXISTS idx_pessoas_concat_trgm_gist;
+
+      CREATE INDEX idx_pessoas_concat_trgm
+      ON pessoas
+      USING GIN (lower(apelido || nome || stack::text) gin_trgm_ops);
+    SQL
+  end
 end
