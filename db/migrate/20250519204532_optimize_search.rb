09f0a4714e70333a5c64c7cf1cb3fd9fbb9a514c
Wagner Caixeta <wagner@baladapp.com.br>
2025-05-20 09:32:44 -0300

    Ajustes

--- db/migrate/20250519204532_optimize_search.rb
+++ db/migrate/20250519204532_optimize_search.rb
@@ -1,24 +1,24 @@
 class OptimizeSearch < ActiveRecord::Migration[8.0]
   def up
-    execute <<~SQL
-      DROP INDEX IF EXISTS idx_pessoas_concat_trgm;
-      -- gera o valor em lower() uma única vez no SELECT ou no índice
-      CREATE EXTENSION IF NOT EXISTS pg_trgm;
+    # execute <<~SQL
+    #   DROP INDEX IF EXISTS idx_pessoas_concat_trgm;
+    #   -- gera o valor em lower() uma única vez no SELECT ou no índice
+    #   CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-      -- índice GiST de trigramas sobre a expressão
-      CREATE INDEX idx_pessoas_concat_trgm_gist
-        ON pessoas
-        USING GIST (lower(apelido || nome || stack::text) gist_trgm_ops);
-    SQL
+    #   -- índice GiST de trigramas sobre a expressão
+    #   CREATE INDEX idx_pessoas_concat_trgm_gist
+    #     ON pessoas
+    #     USING GIST (lower(apelido || nome || stack::text) gist_trgm_ops);
+    # SQL
   end
 
   def down
-    execute <<~SQL
-      DROP INDEX IF EXISTS idx_pessoas_concat_trgm_gist;
+    # execute <<~SQL
+    #   DROP INDEX IF EXISTS idx_pessoas_concat_trgm_gist;
 
-      CREATE INDEX idx_pessoas_concat_trgm
-      ON pessoas
-      USING GIN (lower(apelido || nome || stack::text) gin_trgm_ops);
-    SQL
+    #   CREATE INDEX idx_pessoas_concat_trgm
+    #   ON pessoas
+    #   USING GIN (lower(apelido || nome || stack::text) gin_trgm_ops);
+    # SQL
   end
 end
