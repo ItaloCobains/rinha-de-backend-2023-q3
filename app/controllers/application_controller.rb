999763c3def5a083706c31cd1e96eb600945abad
ItaloCobains <italobrandao.contato@gmail.com>
2025-06-06 20:37:20 -0300

    Add error handling for ActiveRecord exceptions in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: -> { head :unprocessable_entity }
+rescue_from ActiveRecord::RecordNotFound, with: -> { head :unprocessable_entity }
+rescue_from ActiveRecord::RecordNotUnique, with: -> { head :unprocessable_entity }
 end
